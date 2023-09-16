8965d0b00de136003240b2ef9ec0f7e78eaad5dd
Alison Brunk <alisonbrunk@Alisons-MacBook-Pro.local>
2023-09-16 15:44:17 -0600

    one change

--- src/slices/Hero/index.js
+++ src/slices/Hero/index.js
@@ -10,7 +10,7 @@ const components = {
   heading1: ({ children }) => (
     <Heading as="h2" size="xl" className="mb-4 mt-12 first:mt-0 last:mb-0">
       {children}
-    </Heading>
+    </Heading> 
   ),
 };
 
